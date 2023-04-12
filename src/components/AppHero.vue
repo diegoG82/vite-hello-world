e3dba3c3d8fd9f89ef03007627880a3369117cee
diegoG82 <diegogiardina@hotmail.it>
2023-04-12 15:44:25 +0200

    Project complete

---
+++ src/components/AppHero.vue
@@ -0,0 +1,15 @@
+<script>
+export default{
+    name: "AppHero",
+    data(){
+        return{
+           
+        }
+    }
+}
+</script>
+<template>
+   <img src="../assets/img/retro.jpg" alt="">
+</template>
+<style>
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,14 @@
+<script>
+export default{
+    name: "AppTitle",
+    data(){
+        return{
+            title: "Welcome to RetroFuturistic Vite"
+        }
+    }
+}
+</script>
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<style></style>
